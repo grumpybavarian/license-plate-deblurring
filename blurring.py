1a5f64e1077eede0df97613a0fbf0410cdfdcb01
Thomas Edlich <thomas.edlich@tum.de>
2018-02-20 21:40:41 +0000

    adaption to new dataset

--- blurring.py
+++ blurring.py
@@ -7,15 +7,10 @@ import os
 import numpy as np
 import cv2
 import matplotlib.image as mpimg
-# import matplotlib.pyplot as plt
-import matplotlib.pyplot as plt
+from matplotlib import pyplot as plt
 from scipy.misc import imread, imsave, imshow, imresize
 from scipy import ndimage
-from skimage import color, data, restoration
-from scipy.signal import convolve2d as conv2
-import scipy.signal
-
-
+import skimage
 
 def pad_image(img, max_dimensions=(120, 300, 3)):
     # max_dimensions = (78, 245, 3)
@@ -23,7 +18,7 @@ def pad_image(img, max_dimensions=(120, 300, 3)):
     # position padded image randomly
     y_translation = np.random.randint(0, max_dimensions[0] - img.shape[0] + 1)
     x_translation = np.random.randint(0, max_dimensions[1] - img.shape[1] + 1)
-
+   
     y_translation = int((max_dimensions[0] - img.shape[0]) / 2)
     x_translation = int((max_dimensions[1] - img.shape[1]) / 2)
 
@@ -40,7 +35,7 @@ def pad_image(img, max_dimensions=(120, 300, 3)):
 
 def gaussian_blur(img):
     kernel_size = 2 * np.random.randint(1, 9) + 1
-    sigma = np.random.randint(1, 2)
+    sigma = np.random.uniform(0, 3)
 
     return cv2.GaussianBlur(img, (kernel_size, kernel_size), sigma)
 
@@ -119,31 +114,6 @@ def motion_blur(img):
     return images
 
 
-def noise():
-    pass
-
-
-def blurring(src_path, dst_path):
-    for i, f in enumerate(os.listdir(src_path)):
-        print(i)
-        if not f.endswith(".jpg"):
-            continue
-        img = mpimg.imread(os.path.join(src_path, f))
-        gaussian_images = gaussian_blur(img)
-        motion_images = motion_blur(img)
-        gaussian_motion_images = motion_blur(gaussian_images[0])
-
-        for j, gimg in enumerate(gaussian_images):
-            filename = f[:-4] + "_gaussian_{}.jpg".format(j)
-            mpimg.imsave(os.path.join(dst_path, filename), gimg, format="jpg")
-        for j, mimg in enumerate(motion_images):
-            filename = f[:-4] + "_motion_{}.jpg".format(j)
-            mpimg.imsave(os.path.join(dst_path, filename), mimg, format="jpg")
-        for j, img in enumerate(gaussian_motion_images):
-            filename = f[:-4] + "_gaussian_motion_{}.jpg".format(j)
-            mpimg.imsave(os.path.join(dst_path, filename), mimg, format="jpg")
-
-
 def cut_image_from_center(img, shape):
     img_shape = img.shape
     y_translation = int(0.5 * (img_shape[0] - shape[0]))
@@ -154,62 +124,56 @@ def cut_image_from_center(img, shape):
 
 
 def motion_blur2(img):
-    k = np.random.randint(3, 10)
+    k = np.random.randint(10, 20)
     kernel = np.zeros((k, k))
-    point = (int(k/2), int(k/2))
-    num_steps = np.random.randint(10, 20)
-    old_x, x = -2, -2
-    old_y, y = -2, -2
+    point = (int(np.random.normal(0.5 * k, 1)), int(np.random.normal(0.5 * k, 1)))
+    num_steps = k
+    x = np.random.randint(-1, 2)
+    y = np.random.randint(-1, 2)
     for _ in range(num_steps):
         kernel[point] += 1.
-        while x == old_x:
+        if np.random.uniform() < 0.25:
             x = np.random.randint(-1, 2)
-        while y == old_y:
             y = np.random.randint(-1, 2)
-        point = (min(max(0, point[0] + x), k-1), min(max(0, point[1] + y), k-1))
-        old_x = x
-        old_y = y
+        point = (min(max(0, point[0] + x), k - 1), min(max(0, point[1] + y), k - 1))
     kernel = kernel * 1. / np.sum(kernel)
-
-    blurred = cv2.filter2D(img, -1, kernel)
-
+    blurred = cv2.filter2D(img, -1, kernel, borderType = cv2.BORDER_CONSTANT)
     return blurred
 
-def motion_blur3(img):
+def blur_image(img):
     original_shape = img.shape
-
     if img.shape[1] > 150:
-        kernel_size = np.random.randint(12, 20)
+        kernel_size = np.random.randint(5, 18)
     elif img.shape[1] > 100:
-        kernel_size = np.random.randint(10, 15)
+        kernel_size = np.random.randint(4, 14)
     elif img.shape[1] > 75:
-        kernel_size = np.random.randint(5, 11)
+        kernel_size = np.random.randint(3, 11)
     else:
-        kernel_size = np.random.randint(3, 9)
-    kernel_size = np.random.randint(8, 16)
-    angle = np.random.randint(0, 37) * 5
+        kernel_size = np.random.randint(2, 6)
+    angle = np.random.randint(0, 181)
 
     img = ndimage.rotate(img, angle, mode='nearest')
 
     kernel = np.zeros((kernel_size, kernel_size))
-    kernel[int(0.5 * kernel_size), :int(0.5 * kernel_size)] = np.ones(int(0.5 * kernel_size))
+    kernel[int(0.5 * kernel_size), :] = np.ones(kernel_size)
     kernel = kernel * 1. / np.sum(kernel)
     blurred = cv2.filter2D(img, -1, kernel)
 
     blurred = ndimage.rotate(blurred, -angle)
     blurred = cut_image_from_center(blurred, original_shape)
-
+	
+    blurred = gaussian_blur(blurred)
     return blurred
 
 
 def crop(target_img, train_img, crop_size_target=40, crop_size_train=40):
-    if target_img.shape[0] < 40 or target_img.shape[1] < 40:
-        target_img = pad_image(target_img, max_dimensions=(max(crop_size_target, target_img.shape[0]),max(crop_size_target, target_img.shape[1]),3))
-        train_img = pad_image(train_img, max_dimensions=(max(crop_size_target, train_img.shape[0]),max(crop_size_target, train_img.shape[1]),3))
+    if target_img.shape[0] < 40 or target_img.shape[1] < 60:
+        target_img = pad_image(target_img, max_dimensions=(max(crop_size_target, target_img.shape[0]),max(int(1.5*crop_size_target), target_img.shape[1]),3))
+        train_img = pad_image(train_img, max_dimensions=(max(crop_size_target, train_img.shape[0]),max(int(1.5 * crop_size_target), train_img.shape[1]),3))
     x = np.random.randint(0, target_img.shape[0] - crop_size_target + 1)
-    y = np.random.randint(0, target_img.shape[1] - crop_size_target + 1)
-    return [(target_img[x:x+crop_size_target, y:y+crop_size_target],
-             train_img[x:x+crop_size_target, y:y+crop_size_target])]
+    y = np.random.randint(0, target_img.shape[1] - int(1.5 * crop_size_target) + 1)
+    return [(target_img[x:x+crop_size_target, y:y+int(1.5*crop_size_target)],
+             train_img[x:x+crop_size_target, y:y+int(1.5*crop_size_target)])]
     images = []
     margin = int(0.5 * (crop_size_train - crop_size_target))
     train_img = pad_image(train_img,
@@ -228,7 +192,7 @@ def crop(target_img, train_img, crop_size_target=40, crop_size_train=40):
 
 
 def blur_data():
-    train_data_path = "./Data/Original_Train_Data/"
+    train_data_path = "./Data/Training_Images/"
     dst_path_targets = "./Data/Cropped/Clear/"
     dst_path_train = "./Data/Cropped/Blurred/"
 
@@ -237,76 +201,29 @@ def blur_data():
         if "DS_" in f:
             continue
         print(i)
-        os.system('mkdir ./Data/Cropped/Blurred/Training/' + f[:8])
-        os.system('mkdir ./Data/Cropped/Clear/' + f[:8])
-        os.system('mkdir ./Data/Cropped/Blurred/Validation/' + f[:8])
-        dst_path_targets2 = os.path.join(dst_path_targets, f[:8])
-        dst_path_train2 = os.path.join(dst_path_train, f[:8])
+        os.system('mkdir ./Data/Cropped/Blurred/Training/' + f[:6])
+        os.system('mkdir ./Data/Cropped/Clear/' + f[:6])
+        os.system('mkdir ./Data/Cropped/Blurred/Validation/' + f[:6])
+        dst_path_targets2 = os.path.join(dst_path_targets, f[:6])
+        dst_path_train2 = os.path.join(dst_path_train, f[:6])
         img = imread(os.path.join(train_data_path, f))
 
-        for j in range(1, 2):
-            blurred = img
-            if np.random.uniform() < 0.0:
-                blurred = motion_blur2(img)
-            else:
-                blurred = motion_blur3(img)
-
-            if np.random.uniform() <= 0.7:
-                blurred = gaussian_blur(blurred)
-
-            images = crop(img, blurred)
+        for j in range(1, 5):
+            initial_rotate = np.random.randint(-5, 6)
+            r = np.random.uniform(1, 1.5)
+            img_rot = cv2.resize(img, (int(img.shape[1] / r), int(img.shape[0] / r)))
+            img_rot = ndimage.rotate(img_rot, initial_rotate)
+            blurred = blur_image(img_rot)
+            images = crop(img_rot, blurred)
             for pos, (target, train) in enumerate(images):
                 target_filename = f[:-4] + "_pos{:03d}_i{:01d}".format(pos, j) + f[-4:]
                 imsave(os.path.join(dst_path_targets2, target_filename), target)
                 train_filename = f[:-4] + "_pos{:03d}".format(pos) + "_i{:01d}".format(j) + f[-4:]
                 if "_35" in f:
-                    train_filename = os.path.join(dst_path_train, "Validation", f[:8], train_filename)
+                    train_filename = os.path.join(dst_path_train, "Validation", f[:6], train_filename)
                 else:
-                    train_filename = os.path.join(dst_path_train, "Training", f[:8], train_filename)
+                    train_filename = os.path.join(dst_path_train, "Training", f[:6], train_filename)
                 imsave(train_filename, train)
-        # break
-
-def test():
-
-    for f in os.listdir("./Data/Test_Data/"):
-        if "DS_" in f: continue
-        img = imread("./Data/Test_Data/" + f)
-
-        plt.subplot(121)
-        plt.imshow(img)
-        k = np.random.randint(10, 20)
-        kernel = np.zeros((k, k))
-        point = (int(np.random.normal(0.5 * k, 1)), int(np.random.normal(0.5 * k, 1)))
-        num_steps = k
-        x = np.random.randint(-1, 2)
-        y = np.random.randint(-1, 2)
-        for _ in range(num_steps):
-            kernel[point] += 1.
-            if np.random.uniform() < 0.25:
-                x = np.random.randint(-1, 2)
-                y = np.random.randint(-1, 2)
-            point = (min(max(0, point[0] + x), k - 1), min(max(0, point[1] + y), k - 1))
-
-        kernel = kernel * 1. / np.sum(kernel)
-        kernel = np.reshape(kernel, (k,k,1))
-
-        # blurred = cv2.filter2D(img, -1, kernel, borderType = cv2.BORDER_CONSTANT)
-        s = 2
-        kernel = np.array([[-s / 13, -s / 13, -s / 13], [-s / 13, s, -s / 13], [-s / 13, -s / 13, -s / 13]])
-        # blurred = cv2.filter2D(img, -1, kernel)
-
-        blurred = scipy.signal.wiener(img * 1. / 255)
-
-
-        plt.subplot(122)
-        plt.imshow(blurred)
-        blurred *= 255
-        blurred = blurred.astype(np.uint8)
-
-        plt.show()
-
-    return blurred
 
 if __name__ == '__main__':
-    # blur_data()
-    test()
+    blur_data()
--- train.py
+++ train.py
@@ -1,5 +1,4 @@
 import numpy as np
-import cv2
 import os
 os.environ['CUDA_VISIBLE_DEVICES'] = "0"
 import tensorflow as tf
@@ -8,87 +7,58 @@ from datetime import datetime
 from scipy.misc import imread, imsave
 import matplotlib as mpl
 import sys
-from skimage import measure
 
 mpl.use('Agg')
-import matplotlib.pyplot as plt
 
 
 def create_cnn_layer(inputs, n_filters, kernel_size, padding='same', activation=tf.nn.relu, dropout=None):
     cnn = tf.layers.conv2d(inputs=inputs,
-                            filters=n_filters,
-                            kernel_size=[kernel_size, kernel_size],
-                            padding=padding,
-                            activation=activation,
-                            )
+                           filters=n_filters,
+                           kernel_size=[kernel_size, kernel_size],
+                           padding=padding,
+                           activation=activation,
+                           )
     return tf.nn.dropout(cnn, keep_prob=1.0)
 
 
 class Model(object):
     def __init__(self, run_name=None):
-        x_dim_train = 10
-        y_dim_train = 10
-        x_dim_target = 10
-        y_dim_target = 10
-        kernel_sizes = [19, 1, 1, 1, 1, 3, 1, 5, 5, 5, 3, 5, 5, 1, 7, 7, 9]
-        layers = 20
-        kernel_sizes = [15, 10, 6, 2, 1, 2, 6, 10]
+        layers = 25
         kernel_sizes = [5] * layers
-        # kernel_sizes = [9, 1, 5]
-        channel_sizes = [128, 320, 320, 320, 128, 128, 512, 320, 128, 128, 128, 128, 128, 256, 64, 3]
-        channel_sizes = [64] * layers
-        # channel_sizes = [64, 32, 3]
+        channel_sizes = [256] * layers
+
         self.layers = min(len(kernel_sizes), len(channel_sizes))
 
-        self.input = tf.placeholder(tf.float32, shape=(None, None, None, 3))
+        self.input = tf.placeholder(tf.float32, shape=(None, None, None, 1))
         self.dropout = tf.placeholder(tf.float32)
         self.global_step = tf.Variable(0, trainable=False, name='global_step')
 
         self.network = [create_cnn_layer(self.input, channel_sizes[0], kernel_sizes[0], dropout=self.dropout)]
 
         for i in range(1, self.layers):
-            next_layer = create_cnn_layer(self.network[i-1], channel_sizes[i], kernel_sizes[i], dropout=self.dropout)
-            next_layer = tf.add(next_layer, self.network[i-1])
+            next_layer = create_cnn_layer(self.network[i - 1], channel_sizes[i], kernel_sizes[i], dropout=self.dropout)
+            next_layer = tf.add(next_layer, self.network[i - 1])
             self.network.append(next_layer)
 
-        self.predicted_image = create_cnn_layer(self.network[-1], 3, 1, activation=tf.identity, dropout=self.dropout)
+        self.predicted_image = create_cnn_layer(self.network[-1], 1, 1, activation=tf.identity, dropout=self.dropout)
         # long term skip connection
-        # self.predicted_image = tf.add(self.predicted_image, self.input)
+        self.predicted_image = tf.add(self.predicted_image, self.input)
 
-        self.target_image = tf.placeholder(tf.float32, shape=(None, None, None, 3))
+        self.target_image = tf.placeholder(tf.float32, shape=(None, None, None, 1))
         self.loss = tf.nn.l2_loss(tf.subtract(self.target_image, self.predicted_image))
 
-        starter_learning_rate = 1e-5
-        self.learning_rate = tf.train.exponential_decay(starter_learning_rate, self.global_step, \
-                                                        3000, 1., staircase=True)
-
-        self.optimizer = tf.train.AdamOptimizer(learning_rate=self.learning_rate)
-        self.grads = tf.gradients(self.loss, tf.trainable_variables())
-        self.capped_gradients, _ = tf.clip_by_global_norm(self.grads, clip_norm=10)
-        self.grads_and_vars = list(zip(self.capped_gradients, tf.trainable_variables()))
-        self.optimizer = self.optimizer.apply_gradients(self.grads_and_vars, global_step=self.global_step)
-        
         self.optimizer = tf.train.AdamOptimizer(learning_rate=1e-4).minimize(self.loss, global_step=self.global_step)
 
-        # gpu_options = tf.GPUOptions(per_process_gpu_memory_fraction=0.1)
-        # config = tf.ConfigProto(gpu_options=gpu_options)
         config = tf.ConfigProto()
-        # config.gpu_options.allow_growth = True
-        # config.log_device_placement=True
-        
         self.sess = tf.Session(config=config)
 
         self.init = tf.global_variables_initializer()
         self.loss_summary = tf.summary.scalar('train/loss', self.loss)
-        self.lr_summary = tf.summary.scalar('train/learning_rate', self.learning_rate)
         self.validation_loss_summary = tf.summary.scalar('validation/validation_loss', self.loss)
-        train_summaries = [self.loss_summary, self.lr_summary]
-        for grad, var in self.grads_and_vars:
-                print(var.name)
-                # train_summaries.append(tf.summary.histogram("gradients/" + var.name, grad))
+        train_summaries = [self.loss_summary]
+
         self.train_summaries = tf.summary.merge(train_summaries)
         self.validation_summaries = tf.summary.merge([self.validation_loss_summary])
-       
 
         if run_name is None:
             datestring = datetime.strftime(datetime.now(), '%m-%d_%H%M%S')
@@ -112,21 +82,21 @@ class Model(object):
         for root, dirs, files in os.walk(train_data_path):
             for f in files:
                 if f[-3:] in ["jpg", "png"]:
-                    train_data_files.append(os.path.join(train_data_path, f[:8], f))
-                    target_data_files.append(os.path.join(target_data_path, f[:8], f))
+                    train_data_files.append(os.path.join(train_data_path, f[:6], f))
+                    target_data_files.append(os.path.join(target_data_path, f[:6], f))
 
         for root, dirs, files in os.walk(validation_data_path):
             for f in files:
                 if f[-3:] in ["jpg", "png"]:
-                    validation_data_files.append(os.path.join(validation_data_path, f[:8], f))
-                    valdation_target_data_files.append(os.path.join(target_data_path, f[:8], f))
+                    validation_data_files.append(os.path.join(validation_data_path, f[:6], f))
+                    valdation_target_data_files.append(os.path.join(target_data_path, f[:6], f))
 
         all_data_files = np.array(list(zip(train_data_files, target_data_files)))
-        
+
         np.random.shuffle(all_data_files)
         all_validation_files = np.array(list(zip(validation_data_files, valdation_target_data_files)))
 
-        batch_size = 1
+        batch_size = 100
         validation_batch_size = 1
 
         num_batches = int(np.ceil(all_data_files.shape[0] * 1. / batch_size))
@@ -145,9 +115,9 @@ class Model(object):
 
         for epoch in range(num_epochs):
             np.random.shuffle(all_data_files)
-            self.predict()
+            # self.predict()
             for batch_number, batch in enumerate(all_data_files):
-                if batch_number % (int(num_batches / 2)) == 0 and epoch + batch_number != 0:
+                if batch_number % (int(num_batches / 1)) == 0 and epoch + batch_number != 0:
                     validation_loss = 0
 
                     for validation_batch_number, validation_batch in enumerate(all_validation_files):
@@ -158,22 +128,29 @@ class Model(object):
 
                         for i, f in enumerate(validation_batch):
                             validation_img = imread(f[0]) * 1. / 255
+                            validation_img = np.reshape(validation_img,
+                                                        newshape=(validation_img.shape[0], validation_img.shape[1], 1))
                             target_validation_img = imread(f[1]) * 1. / 255
-                            validation_data.append(validation_img) 
+                            target_validation_img = np.reshape(target_validation_img,
+                                                               newshape=(target_validation_img.shape[0],
+                                                                         target_validation_img.shape[1], 1))
+
+                            validation_data.append(validation_img)
                             validation_target_data.append(target_validation_img)
 
                         # img = self.sess.run(self.predicted_image, feed_dict={self.input: validation_data})
                         # imsave(os.path.join("./Data/Predictions/Validation/", f[0][-22:]), img[0])
                         img, val_loss = self.sess.run([self.predicted_image, self.loss],
-                                                         feed_dict={self.input: np.array(validation_data),
-                                                                    self.target_image: np.array(validation_target_data),
-                                                                    self.dropout: 1.0})
+                                                      feed_dict={self.input: np.array(validation_data),
+                                                                 self.target_image: np.array(validation_target_data),
+                                                                 self.dropout: 1.0})
                         img = img.clip(min=0, max=1)
-                        imsave(os.path.join("./Data/Predictions/Validation/", f[0][-22:]), 255 * np.vstack([validation_data[0], img[0], validation_target_data[0]]))
+                        # imsave(os.path.join("./Data/Predictions/Validation/", f[0][-22:]),
+                        #       255 * np.vstack([validation_data[0], img[0], validation_target_data[0]]))
                         validation_loss += val_loss
 
                     validation_loss /= num_validation_batches
-                    print("Validation Loss:", validation_loss, " " * 20)
+                    print("Validation Loss:", validation_loss, " " * 50)
 
                     validation_summary = self.sess.run(self.validation_summaries,
                                                        feed_dict={self.loss: validation_loss})
@@ -184,14 +161,14 @@ class Model(object):
                     self.saver.save(self.sess, save_path)
 
                 duration = time.time()
-                data = np.empty(1, dtype=np.float32)
-                target_data = np.empty(1, dtype=np.float32)
+                data = np.empty((batch.shape[0], 40, 60, 1), dtype=np.float32)
+                target_data = np.empty((batch.shape[0], 40, 60, 1), dtype=np.float32)
 
                 for i, f in enumerate(batch):
-                    train_img = imread(f[0])* 1. / 255
+                    train_img = imread(f[0]) * 1. / 255
                     target_img = imread(f[1]) * 1. / 255
-                    data = np.array([train_img])
-                    target_data = np.array([target_img])
+                    data[i] = np.reshape(train_img, newshape=(40, 60, 1))
+                    target_data[i] = np.reshape(target_img, newshape=(40, 60, 1))
 
                 summaries, _, step, loss = self.sess.run(
                     [self.train_summaries, self.optimizer, self.global_step, self.loss],
@@ -200,10 +177,10 @@ class Model(object):
 
                 self.summary_writer.add_summary(summaries, step)
 
-                print("Epoch {} of {}: Batch {} of {}: Loss: {:.5f}, Duration: {:.2f}sec\r".format(
+                print("Epoch {} of {}: Batch {} of {}: Loss: {:.5f}, Duration: {:.2f}sec".format(
                     epoch, num_epochs, batch_number, num_batches, loss, duration), end="\r")
 
-    def predict(self, restore_path=None, test_data_dir="./Data/Test_Data/",
+    def predict(self, restore_path=None, test_data_dir="./Data/Test_Images/",
                 prediction_dir="./Data/Predictions/"):
         if restore_path is not None:
             self.saver.restore(self.sess, restore_path)
@@ -211,10 +188,13 @@ class Model(object):
         for f in os.listdir(test_data_dir):
             if f.endswith(".jpg") or f.endswith(".png"):
                 img = np.array([imread(os.path.join(test_data_dir, f)) * 1. / 255])
+                img = np.expand_dims(img, axis=3)
 
                 prediction = self.sess.run(self.predicted_image, feed_dict={self.input: img, self.dropout: 1.0})
                 prediction = prediction.clip(min=0, max=1)
-                imsave("./Data/Predictions/Test/" + f, prediction[0] * 255.)
+                prediction = np.vstack([img[0], prediction[0]]) * 255.
+                prediction = np.reshape(prediction, newshape=(prediction.shape[0], prediction.shape[1]))
+                imsave("./Data/Predictions/Test/" + f, prediction)
 
 
 if __name__ == '__main__':
@@ -224,4 +204,4 @@ if __name__ == '__main__':
     else:
         m = Model(run_name=None)
         m.fit(restore_path=None)
-    # m.predict("./models/10-29_195049.ckpt")
+        # m.predict("./models/10-29_195049.ckpt")
